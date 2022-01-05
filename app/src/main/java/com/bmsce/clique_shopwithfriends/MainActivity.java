55988e9603537a6adeb7a0673f5c114ae608b356
tortsBsus <meganraj251@gmail.com>
2022-01-05 12:00:15 +0530

    Initial commit

---
+++ app/src/main/java/com/bmsce/clique_shopwithfriends/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bmsce.clique_shopwithfriends;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
