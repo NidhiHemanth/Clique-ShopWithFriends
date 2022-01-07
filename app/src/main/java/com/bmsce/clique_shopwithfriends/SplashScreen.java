854cc7990728e53294785c6c76059c32c11be15f
Nidhi <nidhihemanth123@gmail.com>
2022-01-07 11:45:04 +0530

    Splash Screen Set-up

---
+++ app/src/main/java/com/bmsce/clique_shopwithfriends/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.bmsce.clique_shopwithfriends;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_TIME_OUT);
+    }
+
+}
