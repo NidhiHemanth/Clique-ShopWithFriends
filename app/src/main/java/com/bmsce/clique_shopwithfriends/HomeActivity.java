960ad047f1351084ad91c4a3c66e33253da392d5
Nidhi <nidhihemanth123@gmail.com>
2022-01-08 08:59:36 +0530

    Fixed Login of Multiple Accounts

--- app/src/main/java/com/bmsce/clique_shopwithfriends/HomeActivity.java
+++ app/src/main/java/com/bmsce/clique_shopwithfriends/HomeActivity.java
@@ -8,12 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private GoogleSignInClient googleSignInClient;
     private FirebaseUser user;
 
     @Override
@@ -24,6 +28,13 @@ public class HomeActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
         TextView username = (TextView) findViewById(R.id.username);
         TextView email = (TextView) findViewById(R.id.email);
 
@@ -33,6 +44,8 @@ public class HomeActivity extends AppCompatActivity {
         Button sign_out = (Button) findViewById(R.id.sign_out);
         sign_out.setOnClickListener(v -> {
             mAuth.signOut();
+            googleSignInClient.signOut();
+
             Intent intent = new Intent(getBaseContext(), LoginActivity.class);
             startActivity(intent);
             finish();
