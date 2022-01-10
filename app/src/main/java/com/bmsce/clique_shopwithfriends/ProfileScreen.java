aa86f8b081a39a70a03f75b7972ace9405613523
tortsBsus <meganraj251@gmail.com>
2022-01-10 21:33:26 +0530

    Activity Creation

--- app/src/main/java/com/bmsce/clique_shopwithfriends/ProfileScreen.java
+++ app/src/main/java/com/bmsce/clique_shopwithfriends/ProfileScreen.java
@@ -3,16 +3,12 @@ package com.bmsce.clique_shopwithfriends;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toast.makeText(getApplicationContext(),"Meg commit 2", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_profile_screen);
     }
-
 }
