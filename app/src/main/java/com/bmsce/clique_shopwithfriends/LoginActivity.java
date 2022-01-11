e91ddda976ec38ae8b9a35bb10d5eb4b27bc981e
tortsBsus <meganraj251@gmail.com>
2022-01-11 20:47:45 +0530

    Button for homeScreen 1

--- app/src/main/java/com/bmsce/clique_shopwithfriends/LoginActivity.java
+++ app/src/main/java/com/bmsce/clique_shopwithfriends/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -46,6 +47,15 @@ public class LoginActivity extends AppCompatActivity {
 
         login = (Button) findViewById(R.id.button);
         login.setOnClickListener(v -> signIn());
+
+        Button X = (Button)(findViewById(R.id.butt));
+        X.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent welcome_activity = new Intent(getBaseContext(), HomeScreen.class);
+                startActivity(welcome_activity);
+            }
+        });
     }
 
     private void signIn() {
