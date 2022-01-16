4e558298d2ccac427036d4a8a33f71f748b0620f
tortsBsus <meganraj251@gmail.com>
2022-01-17 00:45:08 +0530

    Basic Home Screen UI done

--- app/src/main/java/com/bmsce/clique_shopwithfriends/HomeScreen.java
+++ app/src/main/java/com/bmsce/clique_shopwithfriends/HomeScreen.java
@@ -1,49 +1,39 @@
 package com.bmsce.clique_shopwithfriends;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
 import android.widget.Toast;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeScreen extends AppCompatActivity {
     Intent Meghana;
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-
-        Toast.makeText(getApplicationContext(),"Home Screen Reached!", Toast.LENGTH_SHORT).show();
-
-        Button Room = (Button) findViewById(R.id.Rom);
-        Room.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Meghana = new Intent(HomeScreen.this, RoomCodeScreen.class);
-                startActivity(Meghana);
+        Toast.makeText(getApplicationContext(), "Home Screen Reached!", Toast.LENGTH_SHORT).show();
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.getMenu();
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId())
+            {
+                case R.id.rom :   Toast.makeText(getApplicationContext(), "Home Screen Reached!", Toast.LENGTH_SHORT).show();
+                                   break;
+                case R.id.sesh : Meghana = new Intent(HomeScreen.this, SessionsScreen.class);
+                                 startActivity(Meghana);
+                                 break;
+                case R.id.prof : Meghana = new Intent(HomeScreen.this, ProfileScreen.class);
+                                 startActivity(Meghana);
+                                 break;
             }
+            return true;
         });
-        Button Profile = (Button) findViewById(R.id.prof);
-        Profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Meghana = new Intent(HomeScreen.this, ProfileScreen.class);
-                startActivity(Meghana);
-            }
-        });
-        Button Session = (Button) findViewById(R.id.sesh);
-        Session.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Meghana = new Intent(HomeScreen.this, SessionsScreen.class);
-                startActivity(Meghana);
-            }
-        });
-
-
-
-
     }
 }
