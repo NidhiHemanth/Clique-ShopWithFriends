e2c689587e38289a65aca294922ea19dd53734e2
tortsBsus <meganraj251@gmail.com>
2022-01-18 22:24:05 +0530

    Basic Profile UI (check what to enter in between))

--- app/src/main/java/com/bmsce/clique_shopwithfriends/ProfileScreen.java
+++ app/src/main/java/com/bmsce/clique_shopwithfriends/ProfileScreen.java
@@ -8,20 +8,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class ProfileScreen extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class ProfileScreen extends AppCompatActivity {
+    Intent move;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_screen);
-        Toast.makeText(getApplicationContext(),"Profile Screen Reached!", Toast.LENGTH_SHORT).show();
-        Button Settings = (Button) findViewById(R.id.set);
-        Settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent Meghana = new Intent(ProfileScreen.this, SettingsScreen.class);
-                startActivity(Meghana);
+        Toast.makeText(getApplicationContext(), "Home Screen Reached!", Toast.LENGTH_SHORT).show();
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.getMenu();
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId())
+            {
+                case R.id.rom :   move = new Intent(ProfileScreen.this, HomeScreen.class);
+                    startActivity(move);
+                    break;
+                case R.id.sesh : move = new Intent(ProfileScreen.this, SessionsScreen.class);
+                    startActivity(move);
+                    break;
+                case R.id.prof : Toast.makeText(getApplicationContext(), "Profile Screen Reached!", Toast.LENGTH_SHORT).show();
+                    break;
             }
+            return true;
+        });
+
+        Button Settings = (Button) findViewById(R.id.set);
+        Settings.setOnClickListener(v -> {
+            move = new Intent(ProfileScreen.this, SettingsScreen.class);
+            startActivity(move);
         });
     }
 }
