122997c2f44aca757c2e0999086fcbb6e299cc2a
tortsBsus <meganraj251@gmail.com>
2022-01-18 20:05:54 +0530

    Basic Sessions UI without functionality

--- app/src/main/java/com/bmsce/clique_shopwithfriends/SessionsScreen.java
+++ app/src/main/java/com/bmsce/clique_shopwithfriends/SessionsScreen.java
@@ -8,19 +8,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class SessionsScreen extends AppCompatActivity {
+
+    Intent move;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sessions_screen);
         Toast.makeText(getApplicationContext(),"Sessions Screen Reached!", Toast.LENGTH_SHORT).show();
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.getMenu();
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId())
+            {
+                case R.id.rom :  move = new Intent(SessionsScreen.this, HomeScreen.class);
+                    startActivity(move); break;
+                case R.id.sesh :  Toast.makeText(getApplicationContext(), "Sessions Screen Reached!", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.prof : move = new Intent(SessionsScreen.this, ProfileScreen.class);
+                    startActivity(move);
+                    break;
+            }
+            return true;
+        });
         Button IndiSession = (Button) findViewById(R.id.ind);
         IndiSession.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent Meghana = new Intent(SessionsScreen.this, IndividualSessionScreen.class);
-                startActivity(Meghana);
+                Intent move = new Intent(SessionsScreen.this, IndividualSessionScreen.class);
+                startActivity(move);
             }
         });
     }
