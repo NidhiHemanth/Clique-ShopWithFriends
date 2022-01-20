4d2e98473de0cf0a9bbaf8e3b0911a8bef86242c
Nidhi <nidhihemanth123@gmail.com>
2022-01-20 16:26:36 +0530

    Failed attempt at adding Fragments

--- app/src/main/java/com/bmsce/clique_shopwithfriends/HomeScreen.java
+++ app/src/main/java/com/bmsce/clique_shopwithfriends/HomeScreen.java
@@ -10,8 +10,11 @@ import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeScreen extends AppCompatActivity {
-    Intent Meghana;
+    // Intent Meghana;
     BottomNavigationView bottomNavigationView;
+    HomeFragment homeFrag;
+    SessionFragment seshFrag;
+    ProfileFragment profFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +24,28 @@ public class HomeScreen extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setBackground(null);
         bottomNavigationView.getMenu();
+
+        homeFrag = new HomeFragment();
+        seshFrag = new SessionFragment();
+        profFrag = new ProfileFragment();
+
         bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId())
             {
-                case R.id.rom :   Toast.makeText(getApplicationContext(), "Home Screen Reached!", Toast.LENGTH_SHORT).show();
-                                   break;
-                case R.id.sesh : Meghana = new Intent(HomeScreen.this, SessionsScreen.class);
-                                 startActivity(Meghana);
-                                 break;
-                case R.id.prof : Meghana = new Intent(HomeScreen.this, ProfileScreen.class);
-                                 startActivity(Meghana);
-                                 break;
+                case R.id.rom :
+                    // Toast.makeText(getApplicationContext(), "Home Screen Reached!", Toast.LENGTH_SHORT).show();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFrag).commit();
+                    break;
+                case R.id.sesh :
+                    // Meghana = new Intent(HomeScreen.this, SessionsScreen.class);
+                    // startActivity(Meghana);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, seshFrag).commit();
+                    break;
+                case R.id.prof :
+                    // Meghana = new Intent(HomeScreen.this, ProfileScreen.class);
+                    // startActivity(Meghana);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, profFrag).commit();
+                    break;
             }
             return true;
         });
