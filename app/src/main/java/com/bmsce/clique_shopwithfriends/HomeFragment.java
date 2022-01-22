c9af7fe7ffc258b78277ed927e6728d1014d6f37
Nidhi <nidhihemanth123@gmail.com>
2022-01-22 08:55:26 +0530

    Enter Website only for Host

--- app/src/main/java/com/bmsce/clique_shopwithfriends/HomeFragment.java
+++ app/src/main/java/com/bmsce/clique_shopwithfriends/HomeFragment.java
@@ -9,13 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class HomeFragment extends Fragment {
 
     View view;
+    private EditText edit;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -27,18 +31,46 @@ public class HomeFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_home, container, false);
         Log.d("me->", "home fragment");
 
+        edit = view.findViewById(R.id.WEBSITE);
+        actv(false);
+
+        RadioGroup rg = view.findViewById(R.id.radioGroup);
+
+        rg.setOnCheckedChangeListener((group, checkedId) -> {
+            switch (checkedId) {
+                case R.id.radioButton:
+                    actv(true);
+                    break;
+                case R.id.radioButton2:
+                    actv(false);
+                    break;
+            }
+        });
+
         Button room = view.findViewById(R.id.Enter);
         room.setOnClickListener(v -> {
 
             EditText token = view.findViewById(R.id.TOKEN);
             EditText session = view.findViewById(R.id.SESSION_ID);
+
+            RoomCodeScreen.website = edit.getText().toString();
+
             OpenTokConfig.TOKEN = token.getText().toString();
             OpenTokConfig.SESSION_ID = session.getText().toString();
+
             Intent intent = new Intent(getActivity().getBaseContext(), RoomCodeScreen.class);
             startActivity(intent);
+
             Log.d("me->", "intent");
 
         });
         return view;
+    }
+
+    private void actv(final boolean active) {
+        edit.setEnabled(active);
+        if (active) {
+            edit.requestFocus();
+        }
     }
 }
